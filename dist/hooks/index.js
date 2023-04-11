2968e58faea3ccf8de2147f541e4ffa715e6fa70
Tucker <tucker@graftonstudio.com>
2023-04-11 19:12:46 -0400

    updates

--- dist/hooks/index.js
+++ dist/hooks/index.js
@@ -3,28 +3,8 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.usePrevious = exports.useInterval = void 0;
+exports.usePrevious = void 0;
 var _react = require("react");
-var useInterval = function useInterval(callback, delay) {
-  var savedCallback = (0, _react.useRef)();
-  (0, _react.useEffect)(function () {
-    savedCallback.current = callback;
-  });
-  (0, _react.useEffect)(function () {
-    var tick = function tick() {
-      if (typeof (savedCallback === null || savedCallback === void 0 ? void 0 : savedCallback.current) === 'function') {
-        savedCallback.current();
-      }
-    };
-    if (delay !== null) {
-      var id = setInterval(tick, delay);
-      return function () {
-        return clearInterval(id);
-      };
-    }
-  }, [delay]);
-};
-exports.useInterval = useInterval;
 var usePrevious = function usePrevious(value) {
   var ref = (0, _react.useRef)();
   (0, _react.useEffect)(function () {
