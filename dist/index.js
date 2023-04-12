720253089aaec9509e0212f5d9b42676242befb3
Tucker <tucker@graftonstudio.com>
2023-04-12 10:58:15 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,9 @@ function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && 
 var calculateDigitWidth = function calculateDigitWidth(digit) {
   switch (digit) {
     case '1':
-      return '80%';
+      return '50%';
     case '7':
-      return '87.5%';
+      return '80%';
     default:
       return '100%';
   }
