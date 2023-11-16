97b456700563b623b6f21c972f6f0f97db9da338
Tucker <tucker@graftonstudio.com>
2023-11-16 12:09:08 -0500

    updates

---
+++ rollup.config.mjs
@@ -0,0 +1,30 @@
+export default [
+  {
+    input: 'src/index.ts',
+    external: ['react', 'react-dom'],
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/esm/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', sourceMap: false }),
+      postcss(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
+  },
+];
